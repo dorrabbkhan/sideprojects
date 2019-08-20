96555b28c7bbb7e4608c51000574a8753c4b095b
Dorrabb Khan <dorrabbk@gmail.com>
2019-08-21 01:49:25 +0500

    settings.py: added settings for bullets

--- Alien Invasion/settings.py
+++ Alien Invasion/settings.py
@@ -7,8 +7,14 @@ class Settings:
 
         self.screen_width = 600
         self.screen_height = 600
-        self.bg_color = (200,200,200)
+        self.bg_color = (200, 200, 200)
         # set screen's resolution and bg color
 
         self.ship_speed = 1.5
         # set ship's speed
+
+        self.bullet_speed = 1.0
+        self.bullet_height = 20
+        self.bullet_width = 5
+        self.bullet_color = (100, 100, 100)
+        # set settings for each bullet
